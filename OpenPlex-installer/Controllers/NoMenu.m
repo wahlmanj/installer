e1a7d158e373fe4569fa876f867284d79f7d7fb8
wahlmanj <wahlman.j@gmail.com>
2016-05-05 22:38:34 -0500

    test fix

--- OpenPlex-installer/Controllers/NoMenu.m
+++ OpenPlex-installer/Controllers/NoMenu.m
@@ -16,18 +16,15 @@
 
 - (IBAction)ClickedInstall:(id)sender {
     [guideIP setStringValue:@"Installing CLT or Python and git \nThis can take some time, so please be patient..."];
-
     NSString* path1 = [[NSBundle mainBundle] pathForResource:@"Part1" ofType:@"scpt"];
     NSURL* url1 = [NSURL fileURLWithPath:path1];NSDictionary* errors = [NSDictionary dictionary];
     NSAppleScript* appleScript1 = [[NSAppleScript alloc] initWithContentsOfURL:url1 error:&errors];
     [appleScript1 executeAndReturnError:NULL];
     yourTimer=[NSTimer scheduledTimerWithTimeInterval:0.5 target:self selector:@selector(setProgressLabel) userInfo:nil repeats:YES];
-    
 }
 
 -(void)setProgressLabel{
     [guideIP setStringValue:[self progressLabelString]];
-    
     if ([[self progressLabelString] isEqualToString: @"Cloning OpenPlex Complete"]) {
         [yourTimer invalidate];
         [guideIP setStringValue:@"Installing OpenPlex into menubar\nThis should be relatively quick, but still be patient..."];
@@ -35,25 +32,21 @@
         NSURL* url3 = [NSURL fileURLWithPath:path3];NSDictionary* errors = [NSDictionary dictionary];
         NSAppleScript* appleScript3 = [[NSAppleScript alloc] initWithContentsOfURL:url3 error:&errors];
         [appleScript3 executeAndReturnError:NULL];
+        [guideIP setStringValue:@"OpenPlex installed :) \nClick OpenPlex icon in menubar to continue..."];
     }else{
     }
 }
 
-- (NSString *)progressLabelString
-{
-    
+- (NSString *)progressLabelString{
     NSString* path2 = [[NSBundle mainBundle] pathForResource:@"ProgressLabel" ofType:@"scpt"];
     NSURL* url2 = [NSURL fileURLWithPath:path2];NSDictionary* errors = [NSDictionary dictionary];
     NSAppleScript* appleScript2 = [[NSAppleScript alloc] initWithContentsOfURL:url2 error:&errors];
     [appleScript2 executeAndReturnError:NULL];
     
-    
     NSDictionary* errorDict;
     NSAppleEventDescriptor *returnDescriptor = NULL;
-    
     returnDescriptor = [appleScript2 executeAndReturnError: &errorDict];
     returnString = [returnDescriptor stringValue];
-    
     return returnString;
     
     
